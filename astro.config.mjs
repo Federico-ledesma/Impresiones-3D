d961579dd25a0be74d2d9ccba50a5f5967600d09
NoxiusDev <federicoledesma542@gmail.com>
2026-09-25 14:47:43 -0300

    Configurar proyecto para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Federico-ledesma.github.io',
+  base: '/Impresiones-3D',
+
   vite: {
     plugins: [tailwindcss()]
   }
