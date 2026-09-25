e6fa40183c5cf0a55da7b1444338f3222419b4af
NoxiusDev <federicoledesma542@gmail.com>
2026-09-25 14:42:52 -0300

    Version 1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  // 1. La dirección URL final donde se va a ver tu página
-  site: 'https://github.io',
-
-  // 2. El nombre exacto de tu repositorio en GitHub con barras laterales
-  base: '/Impresiones-3D/',
-
   vite: {
     plugins: [tailwindcss()]
   }
